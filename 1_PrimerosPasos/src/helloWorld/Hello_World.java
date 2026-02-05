fd4ea47af972dd2432fac4a44dfd8c33ea8df3d0
Adri-Coding-Dev <adricoding647@gmail.com>
2026-02-06 00:10:37 +0100

    Agregamos el Tema 1 referente a los primeros pasos, Hello World! en Java

---
+++ 1_PrimerosPasos/src/helloWorld/Hello_World.java
@@ -0,0 +1,11 @@
+package helloWorld;
+/*PRIMEROS PASOS: MOSTRAR "HELLO WORLD!" en la consola*/
+public class Hello_World {
+
+	public static void main(String[] args) {
+		//Para imprimir texto por pantalla, usamos la siguiente estructura:
+		//System.out.println("Texto que quieras mostrar");
+		System.out.println("Hello World!");
+	}
+
+}
