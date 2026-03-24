dcb1830a8c14035b6fecf8bc05dac395e6eb5907
Adri-Coding-Dev <adricoding647@gmail.com>
2026-03-24 16:58:20 +0100

    Quitamos el ZIP del tema 15

--- 15_GUI/src/main/java/utils/Utils.java
+++ 15_GUI/src/main/java/utils/Utils.java
@@ -12,28 +12,6 @@ import java.util.regex.Pattern;
 public class Utils {
 
     /**
-     * Metodo para mostrar el menu
-     * @return -> Opcion del usuario
-     */
-    public static int menu(){
-        Scanner s = new Scanner(System.in);
-
-        System.out.println("=".repeat(100));
-        System.out.println("OPCIONES");
-        System.out.println("=".repeat(100));
-
-        System.out.println("0.- Salir");
-        System.out.println("1.- Mostrar Todos los Buses  | " + "6.- Mostrar Todos los Conductores    | " + "11.- Mostrar Todos los Lugares");
-        System.out.println("2.- Insertar un Bus          | " + "7.- Insertar un Conductor            | " + "12.- Insertar un Lugar");
-        System.out.println("3.- Borrar un Bus (ID)       | " + "8.- Borrar un Conductor (ID)         | " + "13.- Borrar un Lugar (ID)");
-        System.out.println("4.- Modificar un Bus (ID)    | " + "9.- Modificar un Conductor (ID)      | " + "14.- Modificar un Lugar (ID)");
-        System.out.println("5.- Encontrar un Bus (ID)    | " + "10.- Encontrar un Conductor (ID)     | " + "15.- Encontrar un Lugar (ID)");
-        System.out.println("=".repeat(100));
-
-        return s.nextInt();
-    }
-
-    /**
      * Metodo para averiguar el nombre de la BBDD con Expresiones regulares
      * @return -> Nombre de la BBDD
      */
@@ -46,37 +24,5 @@ public class Utils {
             nombreBBDD = matcher.group(1);
         }
         return nombreBBDD;
-    }
-
-    /**
-     * Metodo para pedir un String al usuario
-     * @param msg -> Mensaje de peticion
-     * @return -> Cadena que el usuario ha introducido
-     */
-    public static String pedirCadenaAlUsuario(String msg){
-        System.out.println(msg);
-        Scanner s = new Scanner(System.in);
-        return s.next();
-    }
-
-    /**
-     * Metodo para pedir un numero al usuario
-     * @param msg -> Mensaje de peticion
-     * @return -> Numero que el usuario ha introducido
-     */
-    public static int pedirNumeroAlUsuario(String msg){
-        System.out.println(msg);
-        Scanner s = new Scanner(System.in);
-        return s.nextInt();
-    }
-
-    /**
-     * Metodo para presentar una opcion
-     * @param titulo -> Titulo u opcion que el usuario tiene que ver
-     */
-    public static void mostrarOpcionElegida(String titulo){
-        System.out.println("=".repeat(100));
-        System.out.println(titulo);
-        System.out.println("=".repeat(100));
     }
 }
